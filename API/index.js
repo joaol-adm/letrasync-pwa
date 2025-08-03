7445e6061abef666d74c88ab48697392022a941c
joaol-adm <joaol.adm.consultor@gmail.com>
2025-08-02 21:53:53 -0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -6,7 +6,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const url = `https://lyrics.lewagon.ai/search?artist=${encodeURIComponent(artist)}&title=${encodeURIComponent(title)}`;
+    //const url = `https://lyrics.lewagon.ai/search?artist=${encodeURIComponent(artist)}&title=${encodeURIComponent(title)}`;
+    const endpoint = 'https://letrasync-proxy.vercel.app/api?artist=${encodeURIComponent(artist)}&title=${encodeURIComponent(title)}`';
     const response = await fetch(url);
 
     if (!response.ok) {
